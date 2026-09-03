801e276c1742392b3be23d2b0fc2747244658736
jere <jeremiasbisso10@gmail.com>
2026-09-03 11:06:24 -0300

    Excluir build

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/.qt/deploy_Instron_4467_ddd7fb3efd.cmake
+++
@@ -1,8 +0,0 @@
-include("F:/PP Jeremias Bisso/Proyectos/Fusion/Instron_4467Full/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/Instron_4467-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "F:/PP Jeremias Bisso/Proyectos/Fusion/Instron_4467Full/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/Instron_4467.exe"
-    GENERATE_QT_CONF
-)
--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/Instron_4467.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Instron_4467.
-# This may be replaced when dependencies are built.
